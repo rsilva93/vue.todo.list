17a19dd62f1530b378142e96f068497e9e4f64be
Robson da Silva <sc.robson@gmail.com>
2017-03-29 15:22:10 -0300

    List view and add

--- js/app.js
+++ js/app.js
@@ -1,6 +1,30 @@
 var app = new Vue({
+  
   el: '#app',
+  
   data: {
-    message: 'Hello Vue!'
+    
+    title: 'My VueJS to-do list',
+    
+    model: {
+        text: ''
+    },
+
+    list: [
+        { text: 'Go to work' },
+        { text: 'Code all day' },
+        { text: 'Rocks' },
+    ]
+  },
+
+  methods: {
+      add: function() {
+        
+        this.list.push(this.model);
+        
+        this.model = {
+            text: ''
+        };
+      }
   }
 });
