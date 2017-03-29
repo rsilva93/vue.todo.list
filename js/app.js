2e00725490eab9ad0ce03debaa9eaeb99c32a5ab
Robson da Silva <sc.robson@gmail.com>
2017-03-29 17:59:52 -0300

    Allow to remove items from list

--- js/app.js
+++ js/app.js
@@ -1,30 +1,43 @@
 var app = new Vue({
-  
-  el: '#app',
-  
-  data: {
-    
-    title: 'My VueJS to-do list',
-    
-    model: {
-        text: ''
+
+    el: '#app',
+
+    data: {
+
+        title: 'My VueJS to-do list',
+
+        message: null,
+
+        model: {
+            text: ''
+        },
+
+        list: [
+            { text: 'Go to work' },
+            { text: 'Code all day' },
+            { text: 'Rocks' },
+        ]
     },
 
-    list: [
-        { text: 'Go to work' },
-        { text: 'Code all day' },
-        { text: 'Rocks' },
-    ]
-  },
+    methods: {
+        add: function () {
 
-  methods: {
-      add: function() {
-        
-        this.list.push(this.model);
-        
-        this.model = {
-            text: ''
-        };
-      }
-  }
+            if (!this.model || !this.model.text || !this.model.text.trim()) {
+                this.message = 'Fill the "Your new to-do" field.';
+                return;
+            }
+
+            this.message = null;
+
+            this.list.push(this.model);
+
+            this.model = {
+                text: ''
+            };
+        },
+
+        remove: function (index) {
+            this.list.splice(index, 1);
+        }
+    }
 });
